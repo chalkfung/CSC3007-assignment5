d23c6f5ae1f47bb886f61001b79c1ea5bf0dd65d
chalkfung <chalkfung@gmail.com>
2022-05-27 00:59:46 +0800

    basic routing

---
+++ csc3007-assignment1-psi-reading/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Home of Assignments</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeView',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
