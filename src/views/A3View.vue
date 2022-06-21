875793964d0d732c101e70d8e7ca2ffd3535cf06
chalkfung <chalkfung@gmail.com>
2022-06-22 02:10:39 +0800

    Adjusted legend and color scheme, added more prominent borders and hover over borders

--- src/views/A3View.vue
+++ src/views/A3View.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container fluid>
-    <v-row justify="center" class="py-6">
+    <v-row justify="center" class="py-3">
       <h1>Singapore 2021 Population</h1>
     </v-row>
-    <v-row justify="center" class="py-3">
-      <v-col cols="1"><svg id="legend" height="350" width="100"></svg></v-col>
-      <v-col cols="11"><svg id="map"></svg></v-col>
+    <v-row justify="center" class="py">
+      <svg id="map"></svg>
     </v-row>
+    <v-row justify="center"><svg id="legend" width="480"></svg></v-row>
     <div class="tooltip"></div>
   </v-container>
 </template>
@@ -41,18 +41,29 @@ export default {
         }
         let geopath = d3.geoPath().projection(projection);
         let intervals = [8000, 16000, 24000, 32000, 40000, 48000, 56000, 64000]
+        let interpolateLegend = []
+        for(let i = 0; i < 1; i+=1/(intervals.length+1))
+        {
+          interpolateLegend.push(d3.interpolateWarm(i))
+        }
+        console.log(interpolateLegend)
         let colorScale = d3.scaleThreshold()
           .domain(intervals)
-          .range(d3.schemePuOr[8]);
+          .range(interpolateLegend);
         let legendSVG = d3.select("#legend")
-        let size = 25
+        let size = 50
 
+        legendSVG
+          .append("text")
+          .text("Population")
+          .attr("y", 20)
+          .style("fill", "FFFFFF")
         legendSVG.selectAll("mydots")
           .data(intervals)
           .enter()
           .append("rect")
-          .attr("x", 0)
-          .attr("y", function (d, i) { return 0 + i * (size + 5) }) // 100 is where the first dot appears. 25 is the distance between dots
+          .attr("x", function (d, i) { return 0 + i * size })
+          .attr("y", 25)
           .attr("width", size)
           .attr("height", size)
           .style("fill", function (d) { return colorScale(d) })
@@ -62,17 +73,17 @@ export default {
           .data(intervals)
           .enter()
           .append("text")
-          .attr("x", 0 + size * 1.2)
-          .attr("y", function (d, i) { return 0 + i * (size + 5) + (size / 2) })
+          .attr("x", function (d, i) { return (i+ 0.75) * size })
+          .attr("y", 20 + size * 1.5)
+          .attr("font-size", "75%")
           .style("fill", function (d) { return colorScale(d) })
           .text(function (d) { return d })
-          .attr("text-anchor", "left")
-          .style("alignment-baseline", "middle")
-
+          
+        console.log(legendSVG)
 
         let svg = d3.select("#map")
           .attr("viewBox", "0 0 " + width + " " + height)
-        console.log(data[0].features)
+
         svg.append("g")
           .attr("id", "districts")
           .selectAll("path")
@@ -80,11 +91,19 @@ export default {
           .enter()
           .append("path")
           .attr("d", geopath)
+          .style("stroke-opacity", .25)
+          .style("stroke", "white")
           .attr("fill", (d) => {
             return d.properties.Population == 0 ? "FFFFFF" : colorScale(d.properties.Population)
           })
           .on("mouseover", (event, d) => {
-            //console.log(d)
+            d3.select(event.path[0])
+              .transition()
+              .duration(200)
+              .style("opacity", 1)
+              .style("stroke-opacity", 1)
+              .style("stroke", "red")
+
             d3.select(".tooltip")
               .text(d.properties.Name + ":" + d.properties.Population)
               .style("position", "absolute")
@@ -93,6 +112,12 @@ export default {
               .style("top", (event.pageY) + "px")
           })
           .on("mouseout", () => {
+            d3.select(event.path[0])
+              .transition()
+              .duration(200)
+              .style("stroke-opacity", .25)
+              .style("stroke", "white")
+
             d3.select(".tooltip")
               .text("")
           })
